1bb3b922680ca8f1b2727043636d99fd16e63e91
faspen <2335@holbertonschool.com>
2020-11-17 19:42:20 +0000

    renamed strtok, betty fixes

--- prompt.c
+++ prompt.c
@@ -1,25 +1,6 @@
 #include "shell.h"
 
 /**
-* _strlen -  returns the length of a string
-* @s: string
-*
-* Description: returns the length of a string
-* Return:i
-*/
-
-/*int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}*/
-
-/**
 * prompt - prompt
 *
 * Return: 0
