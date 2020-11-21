109be69902e8d49cf440d9931b8d60887fac35c4
kwhit2 <kfw2@outlook.com>
2020-11-21 19:19:43 +0000

    Updated _execute.c file

--- _execute.c
+++ _execute.c
@@ -1,7 +1,6 @@
 #include "shell.h"
-#include "hope"
 
-/*int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2);
 
 char *builtin_cmd[] = {
 	"cd",
@@ -15,17 +14,17 @@ int (*builtin_func[]) (char **) = {
 	&hsh_help,
 	&hsh_exit,
 	&hsh_env
-};*/
+};
 
 /**
 * hsh_num_builtins - num built in
 *
 * Return: size of & contents of char *
 */
-/*int hsh_num_builtins(void)
+int hsh_num_builtins(void)
 {
 	return ((sizeof(builtin_cmd)) / sizeof(char *));
-}*/
+}
 
 /**
 * hsh_help - help built in
@@ -34,18 +33,16 @@ int (*builtin_func[]) (char **) = {
 *
 * Return: 1
 */
-int hsh_help(void)
+int hsh_help(char **args)
 {
+	int i;
+
 	my_puts("Finn Aspenson and Kyle Whitten's simple shell");
 	my_puts("Type command names and arguments, then hit enter");
 	my_puts("The following commands have been built in:\n");
-<<<<<<< HEAD
-	my_puts(" cd\n help\n exit\n env\n");
-=======
 	for (i = 0; i < hsh_num_builtins(); i++)
 		my_puts(" builtin_cmd[i]\n");
 	args = args;
->>>>>>> kyle
 	my_puts("Use man command for more info.\n");
 	return (1);
 }
@@ -57,23 +54,19 @@ int hsh_help(void)
   */
 int hsh_execute(char **args)
 {
-	pid_t my_pid;
-	int status, exec_stat;
+	int i;
 
-	my_pid = fork();
+	if (args[0] == NULL)
+	{
+		return (1);
+	}
 
-	if (my_pid == -1)
+	for (i = 0; i < hsh_num_builtins(); i++)
 	{
-		perror("Something went wrong :(\n");
-		exit(EXIT_FAILURE);
+		if (_strcmp(args[0], builtin_cmd[i]) == 0)
+		{
+			return ((*builtin_func[i])(args));
+		}
 	}
-	if (my_pid == 0)
-	{
-		exec_stat = execve(args[0], args, NULL);
-		if (exec_stat == -1)
-			return (-1);
-	}
-	else
-		wait(&status);
-	return (1);
+	return (hsh_launch(args));
 }
